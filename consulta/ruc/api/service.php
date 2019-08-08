9f80169e0e46ceb74adfb023f1c38744fe15fe98
Marcelo <xReedit@gmail.com>
2019-08-08 12:35:58 -0500

    service sunat

--- consulta/ruc/api/service.php
+++ consulta/ruc/api/service.php
@@ -11,5 +11,17 @@
 	
 	
 	$ruc = (isset($ruc))? $ruc : false;
-	echo $cliente->search( $ruc, true );
+	// echo $cliente->search( $ruc, true );
+
+	// CONSULTA RUC TEMPORAL 080819
+	$url = 'http://appx.papaya.com.pe/consultaruc-tempo/consultaruc.tempo.php?ruc='.$ruc;
+
+	$ch = curl_init();
+ 	curl_setopt($ch, CURLOPT_URL,$url);
+
+ 	ob_start();
+    return curl_exec ($ch);
+    ob_end_clean();
+    curl_close ($ch);
+    // CONSULTA RUC TEMPORAL 080819
 ?>
