baae2d561e1ffbbe3b029334b61f5d66963d9dde
marcelo <xreedit@gmail.com>
2020-05-09 00:18:59 -0500

    pruebas 1.6

--- bdphp/leer_excel.php
+++ bdphp/leer_excel.php
@@ -144,7 +144,7 @@ function sheetDataProductos($sheet) {
     if($xrow_producto_new==0){
       $sqlProducto="insert into producto (descripcion,idproducto_familia,codigo_barra,stock_minimo,precio,precio_unitario,precio_venta, idorg, idsede) values ".$re;            
       // echo ' | sql:'.$sqlProducto;
-      //echo 'add producto '.$sqlProducto;
+      echo 'add producto '.$sqlProducto;
       $idProducto=$bdP->xConsulta_UltimoId($sqlProducto);
       $reDP=$reDP.'('.$IdAlmacen.','.$idProducto.','.$row_cant_almacem.','.$fecha_actual.'),';        
       $sqlProductoDetalle=$sqlProductoDetalle."INSERT INTO producto_stock (idproducto, idalmacen, stock ) VALUES (".$idProducto.",".$IdAlmacen.",".$row_cant_almacem."); ";
