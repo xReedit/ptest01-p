c3ec528577194c14ece1b0198c8412193522ef31
marcelo <xreedit@gmail.com>
2021-01-11 12:31:22 -0500

    ruc devuelve *** se pasa a buscar al temporal

--- consulta/ruc/api/service.php
+++ consulta/ruc/api/service.php
@@ -11,7 +11,32 @@
 	
 	
 	$ruc = (isset($ruc))? $ruc : false;
-	echo $cliente->search( $ruc, true );
+	// echo $cliente->search( $ruc, true );
+	$resp = $cliente->search( $ruc, true );
+
+	$respDecode = json_decode($resp, true);
+	$pos = strrpos($respDecode["result"]["RazonSocial"], "****");	
+
+	// 110121 si delvuele *** la razon socual busca en ruta temporañ
+	if (!$pos) {
+		echo $resp;
+	} else {
+
+		// CONSULTA RUC TEMPORAL 080819
+		// se soluciono al 041019
+	
+		$url = 'http://appx.papaya.com.pe/consultaruc-tempo/consultaruc.tempo.php?ruc='.$ruc;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$url);
+
+		ob_start();
+		return curl_exec ($ch);
+		ob_end_clean();
+		curl_close ($ch);
+    	// CONSULTA RUC TEMPORAL 080819
+
+	}
 
 	// CONSULTA RUC TEMPORAL 080819
 	// se soluciono al 041019
