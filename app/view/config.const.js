5fd436df8a5509709ce40f30b88e6620986a26ca
marcelo <xreedit@gmail.com>
2021-01-20 17:14:07 -0500

    notificacion 2

--- app/view/config.const.js
+++ app/view/config.const.js
@@ -3,5 +3,5 @@ var HEADERS_COMPROBANTE_ONLY_AUTH = { 'Authorization': '' }
 
 var URL_SOCKET = 'https://app.restobar.papaya.com.pe';
 var URL_IMG_CARTA = 'https://restobar.papaya.com.pe/file/';
-var URL_SERVER = 'https://app.restobar.papaya.com.pe/api.pwa/v3';
+var URL_SERVER = 'https://app.restobar.papaya.com.pe/api.pwa/v3/';
 var URL_IMG_CORREO = 'https://restobar.papaya.com.pe/repositorio/img_correo/';
