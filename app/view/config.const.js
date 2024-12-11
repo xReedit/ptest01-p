3e44636cd2da7b03fedafddd995a9189e4b98599
xReedit <papaya.restobar@gmail.com>
2024-12-11 15:16:53 -0500

    cpe emitidos

--- app/view/config.const.js
+++ app/view/config.const.js
@@ -1,3 +1,6 @@
+var HEADERS_COMPROBANTE = { 'Content-Type': 'application/json', 'Authorization': ''}
+var HEADERS_COMPROBANTE_ONLY_AUTH = { 'Authorization': '' }	
+
 var URL_SOCKET = 'https://app.restobar.papaya.com.pe'; 
 var URL_IMG_CARTA = 'https://restobar.papaya.com.pe/file/';
 var URL_SERVER = 'https://app.restobar.papaya.com.pe/api.pwa/v3/'; 
